3215465a71cb4a68640a4bfdda910d6d0072511e
aaron <aaonrl@gmail.com>
2017-08-07 17:10:47 +0800

    notification content as json

--- push.js
+++ push.js
@@ -4,20 +4,30 @@ var db = require('./dbpg');
 
 module.exports = function (title, newNoti) {
     db.notiLast(function (row) {
-        var content;
         var now = new Date();
-        var newContent = time.local() + ":\n" + newNoti;
+        var newContent = {[time.local()]: newNoti};
+        var array = [];
         if (row) {
             var diffHours = (now - row.date_added) / 3600000;
-            content = row.content + "\n\n" + newContent;
-            db.notiUpdate(row.date_added, content);
-            console.log("diffHours: " + diffHours + ", out: " + content);
+            array = JSON.parse(row.content);
+            console.log(array);
+            array.push(newContent);
+            db.notiUpdate(row.date_added, JSON.stringify(array));
+
+            var content = array.map(function (x) {
+                var key = Object.keys(x)[0];
+                return key + ":\n" + x[key];
+            }).join("\n\n");
+
+            console.log("diffHours: " + diffHours + ", out: \n" + content);
             if (diffHours > 4) {
                 push(title, content);
+                db.notiNew(now, []);
             }
         }
         else {
-            db.notiNew(now, newContent);
+            array.push(newContent);
+            db.notiNew(now, JSON.stringify(array));
         }
     });
 };
